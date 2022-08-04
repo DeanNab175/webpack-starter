ce451a77ff8470b8c269779ddf3df1a7bd87d2ca
DeanNab175 <36784137+DeanNab175@users.noreply.github.com>
2022-08-04 17:51:12 +0400

    add files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './scss/main.scss'
+import flowerImg from './assets/images/ylanite_koppens.jpg'
+console.log('index.js working')
+
+const imgFlower = document.querySelector('#flowerimg')
+imgFlower.src = flowerImg
+imgFlower.width = '600'
